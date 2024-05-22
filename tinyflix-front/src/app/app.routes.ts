6421dd251cf7ef3bea9df32a4d8f0ddf75effbdb
duolok <dusan.lecic5@gmail.com>
2024-05-22 16:27:57 +0200

    [add] temp path

--- tinyflix-front/src/app/app.routes.ts
+++ tinyflix-front/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SearchComponent } from './pages/search/search.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "sign-up", component: SignUpComponent},
+  { path: "nav-bar", component: NavBarComponent },
   { path: "search", component: SearchComponent, canActivate: [authGuard] },
   { path: "**", component: LoginComponent },
 ];
