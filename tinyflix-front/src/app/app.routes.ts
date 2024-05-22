c5cbde76b809a0a35b6a42172546ff54e14ab7ea
duolok <dusan.lecic5@gmail.com>
2024-05-22 11:48:25 +0200

    [updated] added sign up route

--- tinyflix-front/src/app/app.routes.ts
+++ tinyflix-front/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SearchComponent } from './pages/search/search.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: "login", component: LoginComponent },
+  { path: "sign-up", component: SignUpComponent},
   { path: "search", component: SearchComponent, canActivate: [authGuard] },
   { path: "**", component: LoginComponent },
 ];
