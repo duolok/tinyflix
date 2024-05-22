d471589e3b5ce9072d72a6ce27af9564cd9430f5
duolok <dusan.lecic5@gmail.com>
2024-05-22 16:26:40 +0200

    [add] navbar init

---
+++ tinyflix-front/src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { LoginService } from "../../services/login.service";
+
+@Component({
+  selector: "app-nav-bar",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./nav-bar.component.html",
+  styleUrl: "./nav-bar.component.scss",
+})
+export class NavBarComponent {
+  public role: string = "admin";
+
+  constructor(public router: Router) {}
+
+  ngOnInit(): void {}
+
+  onLogoutClick(): void {
+    localStorage.clear();
+    this.router.navigate(["/home"]);
+  }
+
+}
