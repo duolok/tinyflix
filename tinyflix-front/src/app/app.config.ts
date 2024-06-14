116e3a7d6b4ae253738e5ba39f6c67c4805fe716
duolok <dusan.lecic5@gmail.com>
2024-06-14 15:22:30 +0200

    [add] http config and providers

--- tinyflix-front/src/app/app.config.ts
+++ tinyflix-front/src/app/app.config.ts
@@ -4,10 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     provideAnimations(),
     provideToastr(),
+    provideClientHydration(),
+    provideHttpClient(),
   ],
 };
