35efeebb9f83c2277b10bdaae341e029cc35ce50
duolok <dusan.lecic5@gmail.com>
2024-06-15 16:26:03 +0200

    [add] add router change when card is clicked

--- tinyflix-front/src/app/components/movie-card/movie-card.component.ts
+++ tinyflix-front/src/app/components/movie-card/movie-card.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './movie-card.component.html',
-  styleUrl: './movie-card.component.scss'
+  styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent {
+  @Input() movie: any;
 
+  constructor(private router: Router) {}
+
+  navigateToMovie(): void {
+    this.router.navigate(['/movies', this.movie.name]);
+  }
 }
