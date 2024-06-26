d6c77b80e3a1080a0f5b1dac3c6acfdf493e553f
duolok <dusan.lecic5@gmail.com>
2024-06-26 19:08:27 +0200

    [update] refactored to handle image upload as well

--- tinyflix-back/src/lambda/upload_movie_file/upload_file.py
+++ tinyflix-back/src/lambda/upload_movie_file/upload_file.py
@@ -1,23 +1,42 @@
 import json
 import boto3
 import os
-from utils import create_response
-from s3_service import S3Service
 
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
     try:
         body = json.loads(event['body'])
-        file_name = body['file_name']
-        bucket_name = os.environ['BUCKET_NAME']
-        
-        presigned_url = s3_client.generate_presigned_url(
-            'put_object',
-            Params={'Bucket': bucket_name, 'Key': file_name},
-            ExpiresIn=3600  # URL expiration time in seconds
-        )
-        
-        return create_response(200, json.dumps({'upload_url': presigned_url}))
+        title = body['title']
+        files = body['files']
+        bucket_name = os.environ['MOVIE_BUCKET']
+        upload_urls = []
+
+        for file_name in files:
+            key = f'movies/{title}/{file_name}'
+            presigned_url = s3_client.generate_presigned_url(
+                'put_object',
+                Params={'Bucket': bucket_name, 'Key': key},
+                ExpiresIn=3600
+            )
+            upload_urls.append(presigned_url)
+
+        return create_response(200, json.dumps({'upload_urls': upload_urls}), cors=True)
     except Exception as e:
-        return create_response(500, json.dumps({'error': str(e)}))
+        return create_response(500, json.dumps({'error': str(e)}), cors=True)
+
+def create_response(status_code, body, cors=False):
+    response = {
+        'statusCode': status_code,
+        'body': body,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': '*',
+        }
+    }
+    if cors:
+        response['headers']['Access-Control-Allow-Origin'] = '*'
+    return response
+
