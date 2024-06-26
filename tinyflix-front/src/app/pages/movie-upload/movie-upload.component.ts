24a0e282dae98670312ae2c1dc826b8947e43875
duolok <dusan.lecic5@gmail.com>
2024-06-26 19:19:06 +0200

    [add] handle all logic for uploading movie

--- tinyflix-front/src/app/pages/movie-upload/movie-upload.component.ts
+++ tinyflix-front/src/app/pages/movie-upload/movie-upload.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { UploadService } from '../../services/upload.service';
 
 @Component({
   selector: 'app-movie-upload',
@@ -11,42 +12,93 @@ import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
   styleUrls: ['./movie-upload.component.scss'],
 })
 export class MovieUploadComponent {
-  uploadForm: FormGroup;
-  fileMetadata: any;
 
-  constructor(private fb: FormBuilder) {
+  uploadForm: FormGroup;
+  fileMetadata: any = {};
+  movieFile: File | null = null;
+  imageFile: File | null = null;
+  genresList: string[] = ['Romance', 'Action', 'Horror'];
+
+  constructor(private fb: FormBuilder, private movieUploadService: UploadService) {
     this.uploadForm = this.fb.group({
       title: ['', Validators.required],
       description: ['', Validators.required],
       actors: ['', Validators.required],
       directors: ['', Validators.required],
-      genres: ['', Validators.required],
+      genres: this.fb.array([], Validators.required),
     });
   }
 
-  onFileChange(event: any) {
+  onFileChange(event: any, fileType: string) {
     const file = event.target.files[0];
     if (file) {
-      this.fileMetadata = {
+      this.fileMetadata[fileType] = {
         name: file.name,
         type: file.type,
         size: file.size,
         creationTime: file.lastModifiedDate,
         lastModified: file.lastModifiedDate,
       };
+      if (fileType === 'movie') {
+        this.movieFile = file;
+      } else if (fileType === 'image') {
+        this.imageFile = file;
+      }
+    }
+  }
+
+  onCheckboxChange(event: any) {
+    const genres: FormArray = this.uploadForm.get('genres') as FormArray;
+
+    if (event.target.checked) {
+      genres.push(new FormControl(event.target.value));
+    } else {
+      const index = genres.controls.findIndex(x => x.value === event.target.value);
+      genres.removeAt(index);
     }
   }
 
   onSubmit() {
-    if (this.uploadForm.valid) {
-      const formData = new FormData();
-      formData.append('file', this.fileMetadata);
-      formData.append('title', this.uploadForm.get('title')?.value);
-      formData.append('description', this.uploadForm.get('description')?.value);
-      formData.append('actors', this.uploadForm.get('actors')?.value);
-      formData.append('directors', this.uploadForm.get('directors')?.value);
-      formData.append('genres', this.uploadForm.get('genres')?.value);
+    if (this.uploadForm.valid && this.movieFile && this.imageFile) {
+      const title = this.uploadForm.get('title')?.value;
+      const metadata = {
+        title: this.uploadForm.get('title')?.value,
+        description: this.uploadForm.get('description')?.value,
+        actors: this.uploadForm.get('actors')?.value,
+        directors: this.uploadForm.get('directors')?.value,
+        genres: this.uploadForm.get('genres')?.value,
+        movieFilePath: `movies/${title}/${this.movieFile.name}`,
+        imageFilePath: `movies/${title}/${this.imageFile.name}`
+      };
+      console.log(metadata);
+
+      this.movieUploadService.getPresignedUrls([this.movieFile.name, this.imageFile.name], title).subscribe((response: any) => {
+        const presignedUrls = response.upload_urls;
+        console.log(presignedUrls);
+        console.log("WE GOOD HERE")
+        console.log(presignedUrls[1], "IMAGE PATH")
+        console.log(presignedUrls[0], "FILE PATH")
+
+        if (this.movieFile) {
+          this.movieUploadService.uploadFile(presignedUrls[0], this.movieFile).subscribe(() => {
+            if (this.imageFile) {
+              this.movieUploadService.uploadFile(presignedUrls[1], this.imageFile).subscribe(() => {
+                this.movieUploadService.saveMetadata(metadata).subscribe(metaResponse => {
+                  console.log('Upload successful', metaResponse);
+                }, metaError => {
+                    console.error('Metadata save failed', metaError);
+                  });
+              }, imageError => {
+                  console.error('Image upload failed', imageError);
+                });
+            }
+          }, movieError => {
+              console.error('Movie upload failed', movieError);
+            });
+        }
+      }, urlError => {
+          console.error('Failed to get presigned URLs', urlError);
+        });
     }
   }
 }
-
