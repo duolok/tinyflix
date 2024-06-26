bbf9f827bf37cfd8d5dbccf27b46b5e5d099740a
duolok <dusan.lecic5@gmail.com>
2024-06-26 19:09:42 +0200

    [fix] cors headers

--- tinyflix-back/src/utility/utils.py
+++ tinyflix-back/src/utility/utils.py
@@ -4,7 +4,9 @@ def create_response(status, body):
     return { 
         'statusCode': status, 
         'headers': {
-            'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Headers" : "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
         },
         'body': json.dumps(body)
     }
