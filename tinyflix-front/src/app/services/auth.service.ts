e3c92b3e55873fc52928b4ee540acc5d545c94b1
duolok <dusan.lecic5@gmail.com>
2024-06-29 07:42:30 +0200

    [add] auth token methods

--- tinyflix-front/src/app/services/auth.service.ts
+++ tinyflix-front/src/app/services/auth.service.ts
@@ -1,17 +1,17 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Amplify, Auth } from 'aws-amplify';
 import { cognito } from '../../environments/environment'; 
 
 export interface IUser {
-    email: string;
-    username: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    birthdate: string;
-    role: string;
+  email: string;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  birthdate: string;
+  role: string;
 }
 
 @Injectable({
@@ -19,55 +19,85 @@ export interface IUser {
 })
 export class AuthService {
 
-    constructor(private http: HttpClient) {
-        Amplify.configure({
-            Auth: cognito
-        });
-    }
+  private tokenSubject = new BehaviorSubject<string | null>(null);
+  public token = this.tokenSubject.asObservable();
 
-    public signIn(user: IUser): Promise<any> {
-        return Auth.signIn(user.email, user.password);
-    }
+  constructor(private http: HttpClient) {
+    Amplify.configure({
+      Auth: cognito
+    });
 
-    public signOut(): Promise<any> {
-        localStorage.set('email', '');
-        return Auth.signOut();
-    }
+    this.loadToken();
+  }
 
-    public signUp(user: IUser): Promise<any> {
-        return Auth.signUp({
-            username: user.email,
-            password: user.password,
-            attributes: {
-                birthdate: user.birthdate, 
-                name : user.firstName,
-                family_name: user.lastName,
-                nickname: user.username,
-                'custom:role': 'User',
-            }
-        }).then(result => {
-                console.log("Sign Up Success: ", result);
-                return result;
-            }).catch(error => {
-                console.error("Sign Up Error: ", error);
-                throw error;
-            });
-    }
-    
+  private loadToken() {
+    Auth.currentSession()
+      .then(session => {
+        const token = session.getIdToken().getJwtToken();
+        this.tokenSubject.next(token);
+        localStorage.setItem('token', token);
+      })
+      .catch(() => this.tokenSubject.next(null));
+  }
 
-    public getCurrentUser() : Promise<any> {
-        return Auth.currentUserInfo();
-    }
+  public signIn(user: IUser): Promise<any> {
+    return Auth.signIn(user.email, user.password)
+      .then(user => {
+        this.loadToken();
+        return user;
+      });
+  }
 
-    public updateUser(user: IUser): Promise<any> {
-        return Auth.currentUserPoolUser()
-            .then((cognitoUser: any) => {
-                return Auth.updateUserAttributes(cognitoUser, user);
-            });
-    }
+  public signOut(): Promise<any> {
+    this.tokenSubject.next(null);
+    localStorage.removeItem('token');
+    return Auth.signOut();
+  }
 
+  public signUp(user: IUser): Promise<any> {
+    return Auth.signUp({
+      username: user.email,
+      password: user.password,
+      attributes: {
+        birthdate: user.birthdate,
+        name: user.firstName,
+        family_name: user.lastName,
+        nickname: user.username,
+        'custom:role': user.role
+      }
+    }).then(result => {
+      console.log("Sign Up Success: ", result);
+      return result;
+    }).catch(error => {
+      console.error("Sign Up Error: ", error);
+      throw error;
+    });
+  }
 
-    public verifyUser(user: IUser, code: string) : Promise<any> {
-        return Auth.confirmSignUp(user.email, code);
-    }
+  public getCurrentUser(): Promise<any> {
+    return Auth.currentUserInfo();
+  }
+
+  public updateUser(user: IUser): Promise<any> {
+    return Auth.currentUserPoolUser()
+      .then((cognitoUser: any) => {
+        const attributes = {
+          birthdate: user.birthdate,
+          name: user.firstName,
+          family_name: user.lastName,
+          nickname: user.username,
+          'custom:role': user.role
+        };
+        return Auth.updateUserAttributes(cognitoUser, attributes);
+      });
+  }
+
+  public verifyUser(user: IUser, code: string): Promise<any> {
+    return Auth.confirmSignUp(user.email, code);
+  }
+
+  public getToken(): string | null {
+    return this.tokenSubject.value;
+  }
 }
+
