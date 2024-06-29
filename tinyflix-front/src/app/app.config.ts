1eaa831a329b2e6c5dc88580fabceb018ad58585
duolok <dusan.lecic5@gmail.com>
2024-06-29 17:51:47 +0200

    [add] animations from material added

--- tinyflix-front/src/app/app.config.ts
+++ tinyflix-front/src/app/app.config.ts
@@ -6,12 +6,13 @@ import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     provideAnimations(),
     provideToastr(),
     provideClientHydration(),
-    provideHttpClient(),
+    provideHttpClient(), provideAnimationsAsync(),
   ],
 };
