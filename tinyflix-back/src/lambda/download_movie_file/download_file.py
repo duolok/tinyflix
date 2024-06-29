510214420f0b5df5a153804496dddfc1ed9860dc
duolok <dusan.lecic5@gmail.com>
2024-06-29 07:42:57 +0200

    [add] download file lambda patch

--- tinyflix-back/src/lambda/download_movie_file/download_file.py
+++ tinyflix-back/src/lambda/download_movie_file/download_file.py
@@ -1,22 +1,36 @@
 import json
 import boto3
-from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+from urllib.parse import unquote_plus
+import os
+
+bucket_name = os.environ['MOVIE_BUCKET']
 
 def lambda_handler(event, context):
+    if event['httpMethod'] == 'OPTIONS':
+        return create_response(200, '', cors=True)
+    
     s3_client = boto3.client('s3')
-    bucket_name = 'serverless-movie-bucket'
-    file_key = event['file_key']
+    file_key = unquote_plus(event['queryStringParameters']['file_key'])
 
     try:
-        presigned_url = s3_client.generate_presigned_url('get_object',
-                                                         Params={'Bucket': bucket_name, 'Key': file_key},
-                                                         ExpiresIn=3600)  
-        return {
-            'statusCode': 200,
-            'body': json.dumps({'presigned_url': presigned_url})
-        }
+        presigned_url = s3_client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': bucket_name, 'Key': file_key},
+            ExpiresIn=3600)
+        return create_response(200, json.dumps({'presigned_url': presigned_url}), cors=True)
     except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps(f'Error: {str(e)}')
+        return create_response(500, json.dumps({'error': str(e)}), cors=True)
+
+def create_response(status_code, body, cors=False):
+    response = {
+        'statusCode': status_code,
+        'body': body,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+            'Access-Control-Allow-Methods': 'OPTIONS,GET,POST,PUT,DELETE',
         }
+    }
+    return response
+
