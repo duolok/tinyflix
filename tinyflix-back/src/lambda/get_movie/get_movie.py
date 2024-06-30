d48494898e77fbdea542e2ae31ae21b83a1aa952
duolok <dusan.lecic5@gmail.com>
2024-06-30 19:59:35 +0200

    [add] lambda for getting a single movie

--- tinyflix-back/src/lambda/get_movie/get_movie.py
+++ tinyflix-back/src/lambda/get_movie/get_movie.py
@@ -3,25 +3,30 @@ import os
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(os.environ['MOVIE_TABLE''])
+table = dynamodb.Table(os.environ['MOVIE_TABLE'])
 
 def lambda_handler(event, context):
-    movie_name = event['pathParameters']['name']
+    body = json.loads(event['body'])
+    movie_name = body.get('name')
+    if not movie_name:
+        return create_response(400, json.dumps({'error': 'Movie name is required'}), cors=True)
+
     try:
         response = table.get_item(
             Key={
                 'name': movie_name
             }
         )
-
-    except Exception as e: return create_response(500, json.dumps({'error': str(e)}))
+    except Exception as e: 
+        return create_response(500, json.dumps({'error': str(e)}), cors=True)
 
     if 'Item' not in response:
-        return create_response(404, json.dumps({'error': 'Movie not found'}))
+        return create_response(404, json.dumps({'error': 'Movie not found'}), cors=True)
+    
     body = { 'data': response['Item'] }
-    return create_response(200, json.dumps(body, default=str))
+    return create_response(200, json.dumps(body, default=str), cors=True)
 
-def create_response(status_code, body, cors=False):
+def create_response(status_code, body, cors=True):
     response = {
         'statusCode': status_code,
         'body': body,
