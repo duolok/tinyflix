576862ca71a5e2ed756c878f534c8c2173f7aacd
duolok <dusan.lecic5@gmail.com>
2024-06-30 22:47:04 +0200

    [add] route for movie update

--- tinyflix-front/src/app/app.routes.ts
+++ tinyflix-front/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SearchComponent } from './pages/search/search.component';
 import { MovieUploadComponent } from './pages/movie-upload/movie-upload.component';
+import { MovieUpdateComponent } from './pages/movie-update/movie-update.component';
 import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
 import { authGuard } from './guards/auth.guard';
 
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: "sign-up", component: SignUpComponent},
   { path: "movie/:name", component: MovieDetailComponent },
   { path: "movie-upload", component: MovieUploadComponent},
+  { path: "movie-update/:name", component: MovieUpdateComponent},
   { path: "search", component: SearchComponent, canActivate: [authGuard] },
   { path: "**", component: LoginComponent },
 ];
