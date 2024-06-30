c7f65c16b6f917495b639a4ef6e23ee179a4c331
duolok <dusan.lecic5@gmail.com>
2024-06-30 19:59:23 +0200

    [add] delete file lambda

--- tinyflix-back/src/lambda/delete_movie_file/delete_file.py
+++ tinyflix-back/src/lambda/delete_movie_file/delete_file.py
@@ -7,25 +7,19 @@ s3_client = boto3.client('s3')
 def lambda_handler(event, context):
     try:
         body = json.loads(event['body'])
-        movie_path = body['moviePath']
+        movie_path = body['url']
         bucket_name = os.environ['MOVIE_BUCKET']
-        folder_name = '/'.join(movie_path.split('/')[:-1]) + '/'
         
-        objects_to_delete = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
-        if 'Contents' in objects_to_delete:
-            delete_objects = [{'Key': obj['Key']} for obj in objects_to_delete['Contents']]
-            response = s3_client.delete_objects(
-                Bucket=bucket_name,
-                Delete={'Objects': delete_objects}
-            )
-            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-                return create_response(200, json.dumps({'message': 'Folder and its contents deleted successfully.'}))
-            else:
-                return create_response(500, json.dumps({'error': 'Failed to delete folder and its contents.'}))
-        else:
-            return create_response(404, json.dumps({'error': 'Folder not found.'}))
+        objects_to_delete = s3_client.list_objects(Bucket=bucket_name, Prefix=movie_path)
+        delete_keys = {'Objects': []}
+        delete_keys['Objects'] = [{'Key': obj['Key']} for obj in objects_to_delete.get('Contents', [])]
+        if delete_keys['Objects']:
+            s3_client.delete_objects(Bucket=bucket_name, Delete=delete_keys)
+
+        return create_response(200, json.dumps({'message': 'Movie file deleted successfully.'}), cors=True)
     except Exception as e:
-        return create_response(500, json.dumps({'error': str(e)}))
+        print(f"Error: {str(e)}")
+        return create_response(500, json.dumps({'error': str(e)}), cors=True)
 
 def create_response(status_code, body, cors=False):
     response = {
@@ -41,3 +35,4 @@ def create_response(status_code, body, cors=False):
     if cors:
         response['headers']['Access-Control-Allow-Origin'] = '*'
     return response
+
