f6ebd8f11b1211bd128a9caf3f082b415a6de71f
duolok <dusan.lecic5@gmail.com>
2024-07-01 00:37:15 +0200

    [add] updated routes with search and home

--- tinyflix-front/src/app/app.routes.ts
+++ tinyflix-front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SearchComponent } from './pages/search/search.component';
+import { FilterComponent } from './pages/filter/filter.component';
 import { MovieUploadComponent } from './pages/movie-upload/movie-upload.component';
 import { MovieUpdateComponent } from './pages/movie-update/movie-update.component';
 import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
@@ -13,6 +14,7 @@ export const routes: Routes = [
   { path: "movie/:name", component: MovieDetailComponent },
   { path: "movie-upload", component: MovieUploadComponent},
   { path: "movie-update/:name", component: MovieUpdateComponent},
-  { path: "search", component: SearchComponent, canActivate: [authGuard] },
+  { path: "search", component: FilterComponent, canActivate: [authGuard]},
+  { path: "home", component: SearchComponent, canActivate: [authGuard] },
   { path: "**", component: LoginComponent },
 ];
