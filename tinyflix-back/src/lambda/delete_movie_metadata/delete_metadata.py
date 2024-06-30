620b284e3af53ca32c8598272f4dac9142386fec
duolok <dusan.lecic5@gmail.com>
2024-06-30 19:59:11 +0200

    [add] lambda works, but integration with front fails [require fix]

--- tinyflix-back/src/lambda/delete_movie_metadata/delete_metadata.py
+++ tinyflix-back/src/lambda/delete_movie_metadata/delete_metadata.py
@@ -7,20 +7,15 @@ table = dynamodb.Table(os.environ['MOVIE_TABLE'])
 
 def lambda_handler(event, context):
     try:
-        movie_id = event['pathParameters']['id']
-        response = table.delete_item(
-            Key={
-                'id': movie_id
-            }
-        )
-        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            return create_response(200, json.dumps({'message': 'Movie metadata deleted successfully.'}))
-        else:
-            return create_response(500, json.dumps({'error': 'Failed to delete movie metadata.'}))
+        body = json.loads(event['body'])
+        movie_name = body['movie_name']
+        response = table.delete_item(Key={'name': movie_name})
+        return create_response(200, json.dumps({'message': 'Metadata deleted successfully'}), cors=True)
     except Exception as e:
-        return create_response(500, json.dumps({'error': str(e)}))
+        print(f"Error: {str(e)}")
+        return create_response(500, json.dumps({'error': str(e)}), cors=True)
 
-def create_response(status_code, body, cors=False):
+def create_response(status_code, body, cors=True):
     response = {
         'statusCode': status_code,
         'body': body,
@@ -34,5 +29,4 @@ def create_response(status_code, body, cors=False):
     if cors:
         response['headers']['Access-Control-Allow-Origin'] = '*'
     return response
-
 
