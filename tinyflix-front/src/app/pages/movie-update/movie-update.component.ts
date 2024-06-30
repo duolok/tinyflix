a672fbf644d477e6668ac52b0a58eb54ebc21aa2
duolok <dusan.lecic5@gmail.com>
2024-06-30 22:04:10 +0200

    [add] update movie component init

---
+++ tinyflix-front/src/app/pages/movie-update/movie-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-update.component.html',
+  styleUrl: './movie-update.component.scss'
+})
+export class MovieUpdateComponent {
+
+}
