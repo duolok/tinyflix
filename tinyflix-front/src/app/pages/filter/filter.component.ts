7686cf68c8fd1e178cd1d53eb8158324a746b9c1
duolok <dusan.lecic5@gmail.com>
2024-07-01 19:41:20 +0200

    [add] filter service call

--- tinyflix-front/src/app/pages/filter/filter.component.ts
+++ tinyflix-front/src/app/pages/filter/filter.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
-import { MovieCategoryComponent } from '../../components/movie-category/movie-category.component';
-import { FooterComponent } from '../../components/footer/footer.component';
-import { CommonModule, DatePipe } from "@angular/common";
+import { MovieCardComponent } from '../../components/movie-card/movie-card.component';
+import { CommonModule } from "@angular/common";
+import { Router } from '@angular/router';
+import { MovieService } from '../../services/movie.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [CommonModule, NavBarComponent, MatIconModule, MovieCategoryComponent],
+  imports: [CommonModule, NavBarComponent, MatIconModule, MovieCardComponent, FormsModule],
   templateUrl: './filter.component.html',
-  styleUrl: './filter.component.scss'
+  styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
+  movies: any = [];
+  searchQuery: string = '';
 
+  constructor(private router: Router, private movieService: MovieService) {}
+
+  ngOnInit() {}
+
+  searchMovies() {
+    this.movieService.searchMovies(this.searchQuery).subscribe(
+      response => {
+        console.log('Search response:', response);
+        this.movies = response;
+      },
+      error => {
+        console.error('Error searching movies.', error);
+      }
+    );
+  }
 }
+
