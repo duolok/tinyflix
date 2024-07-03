4ffba594eada210a17f399b5c7ad31010937df1a
duolok <dusan.lecic5@gmail.com>
2024-07-03 18:58:59 +0200

    [add] simple service for handling subscriptions

---
+++ tinyflix-front/src/app/services/subscription.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { cognito } from '../../environments/environment'; 
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubscriptionService {
+  private apiUrl = cognito.apiUrl;
+
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService,
+  ) { }
+
+  subscribe(subscriptionCriteria: any): Observable<any> {
+    return new Observable(observer => {
+      this.authService.getUserId().then(email => {
+        const headers = this.createAuthHeaders();
+        const payload = {
+          userId: email,
+          subscriptionCriteria: subscriptionCriteria
+        };
+        console.log(payload);
+
+        this.httpClient.put(`${this.apiUrl}/content-management`, payload, { headers }).pipe(
+          catchError(error => {
+            console.error('Error subscribing to content.', error);
+            return throwError(error);
+          })
+        ).subscribe(
+          response => {
+            observer.next(response);
+            observer.complete();
+          },
+          error => {
+            observer.error(error);
+          }
+        );
+      }).catch(error => {
+        observer.error(error);
+      });
+    });
+  }
+
+  private createAuthHeaders(): HttpHeaders {
+    const token = this.authService.getToken();
+    if (!token) {
+      throw new Error('Authentication token is missing.');
+    }
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+  }
+}
+
