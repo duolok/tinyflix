7b434efeafb323013949c6e1ae562d022c46c0c0
duolok <dusan.lecic5@gmail.com>
2024-07-05 14:36:15 +0200

    [update] style fixes

--- tinyflix-front/src/app/pages/subscriptions/subscriptions.component.ts
+++ tinyflix-front/src/app/pages/subscriptions/subscriptions.component.ts
@@ -4,7 +4,7 @@ import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { MovieCardComponent } from '../../components/movie-card/movie-card.component';
 import { CommonModule } from "@angular/common";
 import { Router } from '@angular/router';
-import { MovieService } from '../../services/movie.service';
+import { SubscriptionService } from '../../services/subscription.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -12,25 +12,38 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, NavBarComponent, MatIconModule, MovieCardComponent, FormsModule],
   templateUrl: './subscriptions.component.html',
-  styleUrl: './subscriptions.component.scss'
+  styleUrls: ['./subscriptions.component.scss']
 })
-export class SubscriptionsComponent {
-  movies: any = [];
+export class SubscriptionsComponent implements OnInit {
+  genres: string[] = [];
+  actors: string[] = [];
+  directors: string[] = [];
   searchQuery: string = '';
 
-  constructor(private router: Router, private movieService: MovieService) {}
+  constructor(private router: Router, private subscriptionService: SubscriptionService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadSubscriptions();
+  }
 
-  searchMovies() {
-    this.movieService.searchMovies(this.searchQuery).subscribe(
+  loadSubscriptions() {
+    this.subscriptionService.getSubscriptions().subscribe(
       response => {
-        console.log('Search response:', response);
-        this.movies = response;
+        console.log('Subscriptions:', response);
+        const subscriptions = response.data.subscriptions;
+        this.genres = subscriptions.genres || [];
+        this.actors = subscriptions.actors || [];
+        this.directors = subscriptions.directors || [];
       },
       error => {
-        console.error('Error searching movies.', error);
+        console.error('Error loading subscriptions.', error);
       }
     );
   }
+
+  cancelSubscription(type: string, value: string) {
+    console.log(`Canceling subscription for ${type}: ${value}`);
+    // Implement the logic to cancel the subscription
+  }
 }
+
