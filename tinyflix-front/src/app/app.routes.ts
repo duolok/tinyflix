79ecc6f63d6c740fee86c5959e317eaa807be13a
duolok <dusan.lecic5@gmail.com>
2024-07-05 13:23:44 +0200

    [add] subsriptions component | page link

--- tinyflix-front/src/app/app.routes.ts
+++ tinyflix-front/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SearchComponent } from './pages/search/search.component';
 import { FilterComponent } from './pages/filter/filter.component';
+import { SubscriptionsComponent } from './pages/subscriptions/subscriptions.component';
 import { MovieUploadComponent } from './pages/movie-upload/movie-upload.component';
 import { MovieUpdateComponent } from './pages/movie-update/movie-update.component';
 import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
@@ -14,6 +15,7 @@ export const routes: Routes = [
   { path: "movie/:name", component: MovieDetailComponent },
   { path: "movie-upload", component: MovieUploadComponent},
   { path: "movie-update/:name", component: MovieUpdateComponent},
+  { path: "subscriptions", component: SubscriptionsComponent},
   { path: "search", component: FilterComponent, canActivate: [authGuard]},
   { path: "home", component: SearchComponent, canActivate: [authGuard] },
   { path: "**", component: LoginComponent },
