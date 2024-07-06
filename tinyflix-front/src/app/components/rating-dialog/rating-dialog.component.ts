3d1fb86915bfe17390483ceb8363cd9d8b43f308
duolok <dusan.lecic5@gmail.com>
2024-07-06 14:50:16 +0200

    [add] rating dialog logic

---
+++ tinyflix-front/src/app/components/rating-dialog/rating-dialog.component.ts
@@ -0,0 +1,34 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-rating-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './rating-dialog.component.html',
+  styleUrl: './rating-dialog.component.scss'
+})
+export class RatingDialogComponent {
+  movieTitle: string;
+  rating: number = 0;
+
+  constructor(
+    public dialogRef: MatDialogRef<RatingDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.movieTitle = data.movieTitle;
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onRateClick(): void {
+    this.dialogRef.close(this.rating);
+  }
+
+  setRating(rating: number): void {
+    this.rating = rating;
+  }
+}
