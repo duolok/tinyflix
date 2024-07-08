98aae33e04e53c8e519445aa211f7bb73954246d
duolok <dusan.lecic5@gmail.com>
2024-07-08 21:15:39 +0200

    [add] update auth guard

--- tinyflix-front/src/app/guards/auth.guard.ts
+++ tinyflix-front/src/app/guards/auth.guard.ts
@@ -1,20 +1,21 @@
 import { CanActivateFn, Router, UrlTree } from '@angular/router';
-import { LoginService } from '../services/login.service';
 import { inject } from '@angular/core';
+import { LoginService } from '../services/login.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const loginService = inject(LoginService);
-  /*
-    if (userRole == null || !route.data['role'].includes(userRole)) {
-      router.navigate(['home']);
-      return false;
-    }
-*/
 
   if (!loginService.isLoggedIn) {
     return router.createUrlTree(["/login"]);
-  } else {
-    return true;
   }
+
+  const userRole = localStorage.getItem('userRole'); 
+
+  if (!userRole || !route.data['roles'].includes(userRole)) {
+    return router.createUrlTree(["/not-found"]); 
+  }
+
+  return true;
 };
+
