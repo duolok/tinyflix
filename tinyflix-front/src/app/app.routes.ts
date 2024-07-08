21a33353d172a6b4479b80e6b5a1d73a690627fb
duolok <dusan.lecic5@gmail.com>
2024-07-08 21:17:45 +0200

    [add] auth guard to routes

--- tinyflix-front/src/app/app.routes.ts
+++ tinyflix-front/src/app/app.routes.ts
@@ -7,16 +7,18 @@ import { SubscriptionsComponent } from './pages/subscriptions/subscriptions.comp
 import { MovieUploadComponent } from './pages/movie-upload/movie-upload.component';
 import { MovieUpdateComponent } from './pages/movie-update/movie-update.component';
 import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: "sign-up", component: SignUpComponent},
+  { path: "sign-up", component: SignUpComponent },
   { path: "movie/:name", component: MovieDetailComponent },
-  { path: "movie-upload", component: MovieUploadComponent},
-  { path: "movie-update/:name", component: MovieUpdateComponent},
-  { path: "subscriptions", component: SubscriptionsComponent},
-  { path: "search", component: FilterComponent, canActivate: [authGuard]},
-  { path: "home", component: SearchComponent, canActivate: [authGuard] },
-  { path: "**", component: LoginComponent },
+  { path: "movie-upload", component: MovieUploadComponent, canActivate: [authGuard], data: { roles: ['Admin'] } },
+  { path: "movie-update/:name", component: MovieUpdateComponent, canActivate: [authGuard], data: { roles: ['Admin'] } },
+  { path: "subscriptions", component: SubscriptionsComponent, canActivate: [authGuard], data: { roles: ['User', 'Admin'] } },
+  { path: "search", component: FilterComponent, canActivate: [authGuard], data: { roles: ['User', 'Admin'] } },
+  { path: "home", component: SearchComponent, canActivate: [authGuard], data: { roles: ['User', 'Admin'] } },
+  { path: "not-found", component: NotFoundComponent },
+  { path: "**", component: NotFoundComponent },
 ];
